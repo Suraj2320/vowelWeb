04fa402a9157eb4f5d087e6256f9ae5daf349717
Suraj Santosh Pawar <surajpawar913@gmail.com>
2023-04-18 16:46:17 +0530

    Update productRoute.js

--- Backend/src/view/productRoute.js
+++ Backend/src/view/productRoute.js
@@ -1,11 +1,12 @@
 const express=require("express")
 const app=express.Router()
-const {postProduct, deleteProduct, patchProduct, getProduct} = require("../controller/productController")
+const {postProduct, deleteProduct, patchProduct, getProduct,getSingleProduct} = require("../controller/productController")
 
 app.post("/post",postProduct)
 app.delete("/:id",deleteProduct)
 app.patch("/:id",patchProduct)
 app.get("/get",getProduct)
+app.get("/:id",getSingleProduct)
 
 
 module.exports =app
